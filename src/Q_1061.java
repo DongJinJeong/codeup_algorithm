8f748db6619f83727aefe706b243fb33cfb1cc0a
정동진 <jeongdongjin@jeongdongjin-ui-MacBookPro.local>
2020-12-14 16:06:20 +0900

    1070

--- src/Q_1061.java
+++ src/Q_1061.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Q_1061 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        scanner.close();
+
+        int answer = a | b;
+        System.out.println(answer);
     }
 }
--- src/Q_1067.java
+++ src/Q_1067.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Q_1067 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        scanner.close();
+
+        if (x < 0) {
+            System.out.printf("minus\n");
+            if (x % 2 == 0) {
+                System.out.printf("even");
+            } else {
+                System.out.printf("odd");
+            }
+        } else {
+            System.out.printf("plus\n");
+            if (x % 2 == 0) {
+                System.out.printf("even");
+            } else {
+                System.out.printf("odd");
+            }
+        }
+    }
 }
