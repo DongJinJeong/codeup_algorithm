20371856aad6998125f1d333999975325c8228f1
정동진 <jeongdongjin@jeongdongjin-ui-MacBookPro.local>
2020-12-18 12:35:48 +0900

    remove

--- src/Q_1001.java
+++
@@ -1,5 +0,0 @@
-public class Q_1001 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
--- src/Q_1007.java
+++
@@ -1,5 +0,0 @@
-public class Q_1007 {
-    public static void main(String[] args) {
-        System.out.println("\"C:\\Download\\hello.cpp\"");
-    }
-}
